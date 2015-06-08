37aad9d70c32432535bde0c370224dd803e8759b
Jamie Starke <git@jamiestarke.com>
2015-06-07 22:09:32 -0700

    Fixed the revision if statement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@ set -e # exit with nonzero exit code if anything fails
 
 REVISION=`git rev-parse --abbrev-ref HEAD`
 
-if [ "$REVISION" == "master"]; then 
+if [ "$REVISION" == "master" ]; then 
 	git push origin gh-pages
 fi
