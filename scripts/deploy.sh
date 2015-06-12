1b5b321db14f84c8f866a4fb990298fd08aac46d
Jamie Starke <git@jamiestarke.com>
2015-06-12 07:15:39 -0700

    Updated the script to actually use sh syntax

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ set -e # exit with nonzero exit code if anything fails
 
 REVISION=`git rev-parse --abbrev-ref HEAD`
 
-if [ "$REVISION" == "master" ]; then 
+if test "$REVISION" = "master"; then 
 	# For deployment, send the current paster to the Github Pages
 	git push --force origin master:gh-pages
 fi
